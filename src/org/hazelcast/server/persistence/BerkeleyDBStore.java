dcfddcb98389718d1c871c568fa769df54b33c96
wjw465150 <wjw465150@gmail.com>
2012-02-27 14:57:45 +0800

    @wjw_add: 采用BerkeleyDB来实现持久化.

--- src/org/hazelcast/server/persistence/BerkeleyDBStore.java
+++ src/org/hazelcast/server/persistence/BerkeleyDBStore.java
@@ -1,53 +1,34 @@
 package org.hazelcast.server.persistence;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
-
-import org.jboss.serial.io.JBossObjectInputStream;
-import org.jboss.serial.io.JBossObjectOutputStream;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.MapLoaderLifecycleSupport;
 import com.hazelcast.core.MapStore;
-import com.sleepycat.bind.ByteArrayBinding;
 import com.sleepycat.collections.StoredMap;
 import com.sleepycat.je.CheckpointConfig;
 import com.sleepycat.je.Database;
 import com.sleepycat.je.DatabaseConfig;
-import com.sleepycat.je.DatabaseEntry;
 import com.sleepycat.je.Environment;
 import com.sleepycat.je.EnvironmentConfig;
-import com.sleepycat.je.LockMode;
-import com.sleepycat.je.OperationStatus;
 
-public class BerkeleyDBStore<K, V> implements MapLoaderLifecycleSupport, MapStore<K, V> {
+public class BerkeleyDBStore<K, V> implements MapLoaderLifecycleSupport, MapStore<K, V>, Runnable {
   private Environment _env;
   private Database _db; //数据库
+
+  private ScheduledExecutorService _scheduleSync = Executors.newSingleThreadScheduledExecutor(); //同步磁盘的Scheduled
 
   private HazelcastInstance _hazelcastInstance;
   private Properties _properties;
   private String _mapName;
   private Map<K, V> _map;
-
-  private byte[] serialize(Object obj) throws IOException {
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    JBossObjectOutputStream oos = new JBossObjectOutputStream(baos);
-    oos.writeObject(obj);
-    return baos.toByteArray();
-  }
-
-  private Object deserialize(byte[] bb) throws IOException, ClassNotFoundException {
-    ByteArrayInputStream bais = new ByteArrayInputStream(bb);
-    JBossObjectInputStream ois = new JBossObjectInputStream(bais);
-    return ois.readObject();
-
-  }
 
   @Override
   public void init(HazelcastInstance hazelcastInstance, Properties properties, String mapName) {
@@ -55,83 +36,101 @@ public class BerkeleyDBStore<K, V> implements MapLoaderLifecycleSupport, MapStor
     _properties = properties;
     _mapName = mapName;
 
-    synchronized (BerkeleyDBStore.class) {
-      if (_env == null) {
-        EnvironmentConfig envConfig = new EnvironmentConfig();
-        envConfig.setAllowCreate(true);
-        envConfig.setLocking(false);
-        envConfig.setSharedCache(true);
-        envConfig.setTransactional(false);
-        envConfig.setCachePercent(10); //很重要,不合适的值会降低速度
-        envConfig.setConfigParam(EnvironmentConfig.LOG_FILE_MAX, "104857600"); //单个log日志文件尺寸是100M
+    if (_env == null) {
+      EnvironmentConfig envConfig = new EnvironmentConfig();
+      envConfig.setAllowCreate(true);
+      envConfig.setLocking(false);
+      envConfig.setSharedCache(true);
+      envConfig.setTransactional(false);
+      envConfig.setCachePercent(10); //很重要,不合适的值会降低速度
+      envConfig.setConfigParam(EnvironmentConfig.LOG_FILE_MAX, "104857600"); //单个log日志文件尺寸是100M
 
-        File file = new File(System.getProperty("user.dir", ".") + "/db/");
-        if (!file.exists() && !file.mkdirs()) {
-          throw new RuntimeException("Can not create:" + System.getProperty("user.dir", ".") + "/db/");
-        }
-        _env = new Environment(file, envConfig);
+      File file = new File(System.getProperty("user.dir", ".") + "/db/");
+      if (!file.exists() && !file.mkdirs()) {
+        throw new RuntimeException("Can not create:" + System.getProperty("user.dir", ".") + "/db/");
       }
-
-      if (_db == null) {
-        DatabaseConfig dbConfig = new DatabaseConfig();
-        dbConfig.setAllowCreate(true);
-        dbConfig.setDeferredWrite(true); //延迟写
-        dbConfig.setSortedDuplicates(false);
-        dbConfig.setTransactional(false);
-        _db = _env.openDatabase(null, _mapName, dbConfig);
-      }
-
-      if (_map == null) {
-        ByteArrayBinding keyBinding = new ByteArrayBinding();
-        ByteArrayBinding dataBinding = new ByteArrayBinding();
-        this._map = new StoredMap<byte[], byte[]>(_db, keyBinding, dataBinding, true);
-      }
-
+      _env = new Environment(file, envConfig);
     }
+
+    if (_db == null) {
+      DatabaseConfig dbConfig = new DatabaseConfig();
+      dbConfig.setAllowCreate(true);
+      dbConfig.setDeferredWrite(true); //延迟写
+      dbConfig.setSortedDuplicates(false);
+      dbConfig.setTransactional(false);
+      _db = _env.openDatabase(null, _mapName, dbConfig);
+    }
+
+    if (_map == null) {
+      ObjectBinding<K> keyBinding = new ObjectBinding<K>();
+      ObjectBinding<V> dataBinding = new ObjectBinding<V>();
+      this._map = new StoredMap<K, V>(_db, keyBinding, dataBinding, true);
+    }
+
+    int syncinterval = 3;
+    try {
+      syncinterval = Integer.parseInt(_properties.getProperty("syncinterval"));
+    } catch (Exception e) {
+      //e.printStackTrace();
+    }
+    _scheduleSync.scheduleWithFixedDelay(this, 1, syncinterval, TimeUnit.SECONDS);
+    System.out.println(this.getClass().getCanonicalName() + ":" + _mapName + ":初始化完成!");
   }
 
   @Override
   public void destroy() {
-    synchronized (BerkeleyDBStore.class) {
-      if (_db != null) {
-        try {
-          _db.sync();
-        } catch (Throwable ex) {
-          ex.printStackTrace();
-        }
+    _scheduleSync.shutdown();
 
-        try {
-          _db.close();
-        } catch (Throwable ex) {
-          ex.printStackTrace();
-        } finally {
-          _db = null;
-        }
+    if (_db != null) {
+      try {
+        _db.sync();
+      } catch (Throwable ex) {
+        ex.printStackTrace();
       }
 
-      if (_env != null) {
-        try {
-          boolean anyCleaned = false;
-          while (_env.cleanLog() > 0) {
-            anyCleaned = true;
-          }
-          if (anyCleaned) {
-            CheckpointConfig force = new CheckpointConfig();
-            force.setForce(true);
-            _env.checkpoint(force);
-          }
-        } catch (Throwable ex) {
-          ex.printStackTrace();
-        }
-
-        try {
-          _env.close();
-        } catch (Throwable ex) {
-          ex.printStackTrace();
-        } finally {
-          _env = null;
-        }
+      try {
+        _db.close();
+      } catch (Throwable ex) {
+        ex.printStackTrace();
+      } finally {
+        _db = null;
       }
+    }
+
+    if (_env != null) {
+      try {
+        boolean anyCleaned = false;
+        while (_env.cleanLog() > 0) {
+          anyCleaned = true;
+        }
+        if (anyCleaned) {
+          CheckpointConfig force = new CheckpointConfig();
+          force.setForce(true);
+          _env.checkpoint(force);
+        }
+      } catch (Throwable ex) {
+        ex.printStackTrace();
+      }
+
+      try {
+        _env.close();
+      } catch (Throwable ex) {
+        ex.printStackTrace();
+      } finally {
+        _env = null;
+      }
+    }
+
+    System.out.println(this.getClass().getCanonicalName() + ":" + _mapName + ":销毁完成!");
+  }
+
+  @Override
+  //定时将内存中的内容写入磁盘
+  public void run() {
+    try {
+      _db.sync();
+    } catch (Throwable ex) {
+      ex.printStackTrace();
     }
   }
 
@@ -139,53 +138,43 @@ public class BerkeleyDBStore<K, V> implements MapLoaderLifecycleSupport, MapStor
   @Override
   public V load(K key) {
     return this._map.get(key);
-    try {
-      DatabaseEntry entryKey = new DatabaseEntry(serialize(key));
-      DatabaseEntry entryValue = new DatabaseEntry();
-
-      OperationStatus status = _db.get(null, entryKey, entryValue, LockMode.DEFAULT);
-      if (status == OperationStatus.SUCCESS) {
-        return (V) deserialize(entryValue.getData());
-      } else {
-        return null;
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    }
   }
 
   @Override
   public Map<K, V> loadAll(Collection<K> keys) {
     Map<K, V> map = new java.util.HashMap<K, V>(keys.size());
     for (K key : keys) {
-      V value = this.load(key);
-      if (value != null) {
-        map.put(key, value);
-      }
+      map.put(key, this._map.get(key));
     }
     return map;
   }
 
   @Override
   public Set<K> loadAllKeys() {
-    return null;
+    return this._map.keySet();
   }
 
   @Override
   public void delete(K key) {
+    this._map.remove(key);
   }
 
   @Override
   public void deleteAll(Collection<K> keys) {
+    Map<K, V> map = new java.util.HashMap<K, V>(keys.size());
+    for (K key : keys) {
+      map.remove(key);
+    }
   }
 
   @Override
   public void store(K key, V value) {
+    this._map.put(key, value);
   }
 
   @Override
   public void storeAll(Map<K, V> map) {
+    this._map.putAll(map);
   }
 
 }
---
+++ src/org/hazelcast/server/persistence/ObjectBinding.java
@@ -0,0 +1,59 @@
+package org.hazelcast.server.persistence;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.jboss.serial.io.JBossObjectInputStream;
+import org.jboss.serial.io.JBossObjectOutputStream;
+
+import com.sleepycat.bind.EntryBinding;
+import com.sleepycat.je.DatabaseEntry;
+
+public class ObjectBinding<E> implements EntryBinding<E> {
+  /**
+   * Creates a byte Object binding.
+   */
+  public ObjectBinding() {
+  }
+
+  private byte[] serialize(Object obj) throws IOException {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    JBossObjectOutputStream oos = new JBossObjectOutputStream(baos);
+    oos.writeObject(obj);
+    return baos.toByteArray();
+  }
+
+  private Object deserialize(byte[] bb) throws IOException, ClassNotFoundException {
+    ByteArrayInputStream bais = new ByteArrayInputStream(bb);
+    JBossObjectInputStream ois = new JBossObjectInputStream(bais);
+    return ois.readObject();
+
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public E entryToObject(DatabaseEntry entry) {
+
+    int len = entry.getSize();
+    if (len == 0) {
+      return null;
+    } else {
+      try {
+        return (E) deserialize(entry.getData());
+      } catch (Exception e) {
+        e.printStackTrace();
+        return null;
+      }
+    }
+  }
+
+  @Override
+  public void objectToEntry(E object, DatabaseEntry entry) {
+    try {
+      entry.setData(serialize(object));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
