7b7873ac68f1629e03334753d8a1718e03dd4b55
wjw465150 <wjw465150@gmail.com>
2012-05-21 16:17:29 +0800

    @wjw_change: 输出服务器start,stop消息.

--- src/org/hazelcast/server/WrapperHazelcastApp.java
+++ src/org/hazelcast/server/WrapperHazelcastApp.java
@@ -1,8 +1,27 @@
 package org.hazelcast.server;
 
+import org.tanukisoftware.wrapper.WrapperManager;
 import org.tanukisoftware.wrapper.WrapperSimpleApp;
 
 public class WrapperHazelcastApp extends WrapperSimpleApp {
+  @Override
+  public Integer start(String[] args) {
+    Integer result = super.start(args);
+    
+    WrapperManager.log(WrapperManager.WRAPPER_LOG_LEVEL_FATAL, "Started HazelcastServerApp!");
+
+    return result;
+  }
+
+  @Override
+  public int stop(int exitCode) {
+    int result = super.stop(exitCode);
+
+    WrapperManager.log(WrapperManager.WRAPPER_LOG_LEVEL_FATAL, "Stoped HazelcastServerApp!");
+
+    return result;
+  }
+
   protected WrapperHazelcastApp(String[] strings) {
     super(strings);
   }
